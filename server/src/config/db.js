3f3a90eb651f8a07f88f7d2a92f7b1d778eb68a6
Tejas Chaudhari <tejaschaudhari@gmail.com>
2026-07-12 11:17:58 +0530

    neon db connect

---
+++ server/src/config/db.js
@@ -0,0 +1,27 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env"),
+});
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on("connect", () => {
+  console.log("✅ PostgreSQL Connected");
+});
+
+pool.on("error", (err) => {
+  console.error("❌ PostgreSQL Error:", err);
+});
+
+export default pool;
--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+// Test database connection
+app.get("/", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json({
+            success: true,
+            message: "Server & Database Connected",
+            time: result.rows[0].now,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Database connection failed",
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
